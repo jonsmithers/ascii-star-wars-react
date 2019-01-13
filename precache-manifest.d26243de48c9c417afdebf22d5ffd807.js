6e46e238675ec84ad6492c1a8a38f7feb6ddd484
Jon Smithers <mail@jonsmithers.link>
2019-01-12 23:06:49 -0500

    Updates

---
+++ precache-manifest.d26243de48c9c417afdebf22d5ffd807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ascii-star-wars-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e04ebc6315d9e5423780",
+    "url": "/ascii-star-wars-react/static/js/runtime~main.e04ebc63.js"
+  },
+  {
+    "revision": "f148ef13c3ec3b6ea2dd",
+    "url": "/ascii-star-wars-react/static/js/main.f148ef13.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/ascii-star-wars-react/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f148ef13c3ec3b6ea2dd",
+    "url": "/ascii-star-wars-react/static/css/main.88146425.chunk.css"
+  },
+  {
+    "revision": "832dcb836d9a07a1da65f8012402f345",
+    "url": "/ascii-star-wars-react/index.html"
+  }
+];
